e9660225b63f3e2456fc357ea60bd849c5020053
Antoine Nauleau <ajnauleau@gmail.com>
2018-08-02 12:38:19 +0200

    Add square-even-numbers.js

---
+++ Common/Made/square-even-numbers.js
@@ -0,0 +1,16 @@
+// squares each number in an array, then
+// returns the sum
+
+const squareEvenNumbers = (array) => {
+  let sum = 0;
+  for(let i=0; i<array.length; i++) {
+     if(array[i] % 2 == 0) {
+       sum = sum + array[i]**2;
+     }
+  }
+  return sum;
+}
+
+export default squareEvenNumbers;
+
+squareEvenNumbers([1,2,1,4]);
