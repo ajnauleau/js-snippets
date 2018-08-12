61873d7dd43fb33c2c8e451a3d6020218c2aacbe
Antoine Nauleau <ajnauleau@gmail.com>
2018-08-12 10:24:10 -0700

    Finish runningAverage in Made

--- Core/Made/runnningAverage.js
+++ Core/Made/runnningAverage.js
@@ -1,22 +1,8 @@
 
-/*
-function round2Fixed(value) {
-  value = +value;
-
-  if (isNaN(value))
-    return NaN;
-
-  // Shift
-  value = value.toString().split('e');
-  value = Math.round(+(value[0] + 'e' + (value[1] ? (+value[1] + 2) : 2)));
-
-  // Shift back
-  value = value.toString().split('e');
-  return (+(value[0] + 'e' + (value[1] ? (+value[1] - 2) : -2))).toFixed(2);
-}
-*/
-
 const runningAverage = (num) => {
+  function round(value, decimals) {
+    return Number(Math.round(value+'e'+decimals)+'e-'+decimals);
+  }
   let currentAverage = [];
   let lCD = null;
   function internalFunction(num) {
@@ -24,24 +10,23 @@ const runningAverage = (num) => {
       lCD = 1;
       let parsed = Number.parseInt(num, 10);
       currentAverage.push(parsed);
-      let roundAverage = currentAverage[0];
+      let roundAverage = round(currentAverage[0], 2);
       console.log('Init: ' + roundAverage);
-      return roundAverage;//.toFixed(2);
+      return roundAverage;
 
     } else {
       lCD++;
       let parsed = Number.parseInt(num, 10);
       let newSum = (currentAverage[0] + parsed);
       currentAverage[0] = newSum;
-      let roundAverage = currentAverage[0] / lCD;
+      let roundAverage = round(currentAverage[0] / lCD, 2);
       console.log('Build: ' + roundAverage);
-      return roundAverage.toFixed(2);
-
+      return roundAverage;
+      
     }
   }
 
  return internalFunction;
-
 };
 
 rAvg = runningAverage();
